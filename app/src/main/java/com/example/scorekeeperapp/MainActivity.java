ac1461d1042a391f78087fb0db7da2d1cf07bb5a
Javlon <rahimovjavlon1212@gmail.com>
2020-04-03 20:54:02 +0500

    UI is ready

--- app/src/main/java/com/example/scorekeeperapp/MainActivity.java
+++ app/src/main/java/com/example/scorekeeperapp/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.scorekeeperapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+            getWindow().setStatusBarColor(ContextCompat.getColor(this, android.R.color.transparent));
+        }
+
     }
 }
